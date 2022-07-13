3c0289403e3f0623d5d11399c79dadb14bee6dec
Gumato-Roba <gumatorobba@gmail.com>
2022-07-13 21:13:11 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,108 @@
+import random
+
+name = input("Type your name: ")
+print("Welcome", name, "to our Cafee!")
+
+answer = input("What drinks do you want to order? Capuccino/White Tea/Black Tea/ Coffee: ")
+if answer == "Coffee":
+    answer = input("Great!  How many cups of coffee do you want to order? Type the number of cups you want? (1/2/3/4/5/6/7/8/9/10):  ") 
+    
+    if answer == "1":
+       print("Your order will be delivered shortly. Thank you for shopping with us!:")
+    elif answer == "2":
+       print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+    elif answer == "3":
+       print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+    elif answer == "4":
+       print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+    elif answer == "5":
+       print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "6":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "7":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "8":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "9":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "10":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    else:
+      print("Choose another option, Black Tea/ White Tea/ Capuccino")
+elif answer == "White Tea":
+   answer = input("Great!  How many cups of White Tea do you want to order? Type the number of cups you want? (1/2/3/4/5/6/7/8/9/10): ") 
+   if answer == "1":
+      print("Your order will be delivered shortly. Thank you for shopping with us!:")     
+   elif answer == "2":
+      print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+   elif answer == "3":
+      print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+   elif answer == "4":
+      print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+   elif answer == "5":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "6":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "7":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "8":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "9":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "10":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   else:
+      print("Choose another option, Black Tea/ White Tea/ Capuccino")
+elif answer == "Black Tea":
+  answer = input("Great!  How many cups of Black Tea do you want to order? Type the number of cups you want? (1/2/3/4/5/6/7/8/9/10):  ")  
+  if answer == "1":
+      print("Your order will be delivered shortly. Thank you for shopping with us!:")
+  elif answer == "2":
+      print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+  elif answer == "3":
+      print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+  elif answer == "4":
+      print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+  elif answer == "5":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+  elif answer == "6":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+  elif answer == "7":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+  elif answer == "8":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+  elif answer == "9":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+  elif answer == "10":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+  else:
+      print("Thank you, Shop with us again")  
+
+     
+elif answer == "Capuccino":
+   answer = input("Great!  How many cups of Capuccino do you want to order? Type the number of cups you want? (1/2/3/4/5/6/7/8/9/10):  ") 
+   if answer == "1":
+      print("Your order will be delivered shortly. Thank you for shopping with us!:")
+   elif answer == "2":
+      print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+   elif answer == "3":
+      print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+   elif answer == "4":
+      print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+   elif answer == "5":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "6":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "7":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "8":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "9":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "10":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   else:
+      print("Thank you, Shop with us again")
+
+
+
